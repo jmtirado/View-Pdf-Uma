625ec13ddc13158b0cfe2d9e0dfe068cb3bbbb7d
R3tikus <r3tikus@gmail.com>
2014-01-12 23:38:07 +0100

    add all campus

--- download_uma.php
+++ download_uma.php
@@ -1,8 +1,10 @@
 <?php 
 if ($_POST)
 {
+	$cv = array('http://etsit.cv.uma.es/pluginfile.php','http://ccedu.cv.uma.es/pluginfile.php','http://ciencias.cv.uma.es/pluginfile.php','http://politecnica.cv.uma.es/pluginfile.php','http://arquitectura.cv.uma.es/pluginfile.php','http://industriales.cv.uma.es/pluginfile.php','http://informatica.cv.uma.es/pluginfile.php','http://bellasartes.cv.uma.es/pluginfile.php','http://ciencias.cv.uma.es/pluginfile.php','http://cccom.cv.uma.es/pluginfile.php','http://economicas.cv.uma.es/pluginfile.php','http://salud.cv.uma.es/pluginfile.php','http://comerciogestion.cv.uma.es/pluginfile.php','http://derecho.cv.uma.es/pluginfile.php','http://fest.cv.uma.es/pluginfile.php','http://filosofia.cv.uma.es/pluginfile.php','http://medicina.cv.uma.es/pluginfile.php','http://turismo.cv.uma.es/pluginfile.php','http://psicologia.cv.uma.es/pluginfile.php');
 	$data = $_POST['DOM'];
 	$htmlenc = urldecode($data);
+	$urldocument = "";
 
 	error_reporting(E_ERROR | E_PARSE);
 	
@@ -10,11 +12,14 @@ if ($_POST)
 	$domd->loadHTML($htmlenc);
 	foreach ($domd->getElementsByTagName('a') as $node) {
     	$testurl=$node->getAttribute( 'href' );
- 
-    	if (strpos($testurl, 'http://etsit.cv.uma.es/pluginfile.php') === 0) {
-    		$urldocument = $testurl;
-    		
-		}
+		
+		foreach( $cv as $valor)
+		{
+			if (strpos($testurl, $valor ) === 0) {
+				$urldocument = $testurl;
+			}
+		}	
+		
 
 	}
 	echo "Redireccionando...";
